0a9ce49c22fe697d8021bb9a363f36a01f531448
Monkey Prince <kyloren3600@gmail.com>
2025-05-14 01:13:08 +0200

    add spawn blur

--- Assets/Prefabs/Player/HealthSystem.cs
+++ Assets/Prefabs/Player/HealthSystem.cs
@@ -7,16 +7,21 @@ public class HealthSystem : MonoBehaviour {
     // Max health = 100f
     public float maxHealth = 100f;
     public float healthPerSecond = 3;
+    public float spawnEffectTransitionTime = 5f;
     public float vignetteTransitionTime = 0.5f;
 
     private float health;
     private Vignette vignette;
+    private DepthOfField depthOfField;
     private Coroutine transitionCoroutine;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         health = maxHealth;
         Camera.main.GetComponent<PostProcessVolume>().profile.TryGetSettings(out vignette);
+        Camera.main.GetComponent<PostProcessVolume>().profile.TryGetSettings(out depthOfField);
+
+        SpawnEffect();
     }
 
     // Update is called once per frame
@@ -26,7 +31,6 @@ public class HealthSystem : MonoBehaviour {
     }
 
     public void TakeDamage(float damageAmount) {
-        Debug.Log("take damage");
         health -= damageAmount;
         UpdateDisplayedHealth();
     }
@@ -53,5 +57,32 @@ public class HealthSystem : MonoBehaviour {
 
         // Ensure the final intensity is set correctly
         vignette.intensity.value = endIntensity;
+    }
+
+    public void SpawnEffect() {
+        StartCoroutine(SpawnEffect(spawnEffectTransitionTime));
+    }
+
+    IEnumerator SpawnEffect(float duration) {
+        float elapsedTime = 0f;
+        float startIntensity = 0;
+        float endIntensity = 7f;
+
+        depthOfField.active = true;
+        while (elapsedTime < duration) {
+            // Calculate the current intensity using linear interpolation
+            float currentIntensity = Mathf.Lerp(startIntensity, endIntensity, elapsedTime / duration);
+            depthOfField.aperture.value = currentIntensity;
+
+            // Increment the elapsed time
+            elapsedTime += Time.deltaTime;
+
+            // Wait for the next frame
+            yield return null;
+        }
+
+        // Ensure the final intensity is set correctly
+        depthOfField.aperture.value = endIntensity;
+        depthOfField.active = false;
     }
 }
