0538a352cc1917e8d644026c1d1f410a3ac41429
juaniac <juan.iaconucci@epfl.ch>
2025-05-17 23:16:15 +0200

    Juan did his tutorial level

---
+++ Assets/Prefabs/Tutorials/ForceTutorialManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using System.Collections.Generic;
+using TMPro;
+
+[RequireComponent(typeof(InputData))]
+public class ForceTutorialManager : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public Transform spawnPoint;
+    public float spawnRadius = 1f;
+
+	public OnTriggerWatcher spawnTrigger;
+	public OnTriggerWatcher tryPullTrigger;
+	public OnTriggerWatcher tryPullPushComboTrigger;
+
+	public TMP_Text tryForcePushText;
+	public TMP_Text tryForcePullText;
+	public TMP_Text tryForcePullPushComboText;
+	public TMP_Text tryDefeatingAllEnnemiesText;
+
+	public GameObject Ennemy1;
+	public GameObject Ennemy2;
+
+	private InputData _inputData;
+	private TutorialState _currentState;
+
+	public enum TutorialState {
+		TryForcePush,
+		TryForcePull,
+		TryForcePullPushCombo,
+		TryDefeatingEnnemies,
+		Win
+	}
+
+	void Start()
+    {
+        _inputData = GetComponent<InputData>();
+		_currentState = TutorialState.TryForcePush;
+	}
+
+    void Update()
+    {
+		_inputData._leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonPressed);
+        if (buttonPressed && spawnTrigger.IsTriggerEmpty) {
+			SpawnForceObjects();
+        }
+		UpdateState();
+		UpdateUI();
+	}
+
+	private void UpdateState() {
+		if(_currentState == TutorialState.TryForcePush) {
+			if(spawnTrigger.IsTriggerEmpty) {
+				_currentState = TutorialState.TryForcePull;
+			}
+		}else if(_currentState == TutorialState.TryForcePull) {
+			if (!tryPullTrigger.IsTriggerEmpty) {
+				_currentState = TutorialState.TryForcePullPushCombo;
+			}
+		}else if (_currentState == TutorialState.TryForcePullPushCombo) {
+			if (!tryPullPushComboTrigger.IsTriggerEmpty) {
+				_currentState = TutorialState.TryDefeatingEnnemies;
+			}
+		}else if (_currentState == TutorialState.TryDefeatingEnnemies) {
+			if (Ennemy1 == null && Ennemy2 == null) {
+				_currentState = TutorialState.Win;
+			}
+		}else if (_currentState == TutorialState.Win) {
+			//TELEPORT THE PLAYER BACK TO THE MAIN HUB
+		}
+	}
+
+    private void SpawnForceObjects(){
+        if (prefabToSpawn == null || spawnPoint == null)
+            return;
+
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 offset = Random.insideUnitSphere * spawnRadius;
+            offset.y = 0;
+            Instantiate(prefabToSpawn, spawnPoint.position + offset, spawnPoint.rotation);
+        }
+    }
+
+	private void UpdateUI() {
+		if (tryForcePushText != null) tryForcePushText.gameObject.SetActive(_currentState == TutorialState.TryForcePush);
+		if (tryForcePullText != null) tryForcePullText.gameObject.SetActive(_currentState == TutorialState.TryForcePull);
+		if (tryForcePullPushComboText != null) tryForcePullPushComboText.gameObject.SetActive(_currentState == TutorialState.TryForcePullPushCombo);
+		if (tryDefeatingAllEnnemiesText != null) tryDefeatingAllEnnemiesText.gameObject.SetActive(_currentState == TutorialState.TryDefeatingEnnemies);
+		if (Ennemy1 != null) Ennemy1.gameObject.SetActive(_currentState == TutorialState.TryDefeatingEnnemies);
+		if (Ennemy2 != null) Ennemy2.gameObject.SetActive(_currentState == TutorialState.TryDefeatingEnnemies);
+	}
+}
---
+++ Assets/Prefabs/Tutorials/OnTriggerWatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OnTriggerWatcher : MonoBehaviour
+{
+	private HashSet<Collider> _collidersInside = new HashSet<Collider>();
+
+	public bool IsTriggerEmpty => _collidersInside.Count == 0;
+
+	void OnTriggerEnter(Collider other) {
+		if (!other.isTrigger)
+			_collidersInside.Add(other);
+	}
+
+	void OnTriggerExit(Collider other) {
+		_collidersInside.Remove(other);
+	}
+}
