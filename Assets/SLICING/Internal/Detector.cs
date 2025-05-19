19349df993051f9833514bfbd7abb89bbfa1b30d
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2025-05-19 07:52:01 +0200

    Add slicing

---
+++ Assets/SLICING/Internal/Detector.cs
@@ -0,0 +1,93 @@
+using System;
+using Slicing;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using JetBrains.Annotations;
+using UnityEngine;
+
+
+namespace Slicing {
+
+	[RequireComponent(typeof(VelocityTracker))]
+	[RequireComponent(typeof(SlicingKnife))]
+	class SlicingDetector : MonoBehaviour {
+		public float angVelocityThreshold = 7f;
+		public Transform slicerBeginEffector;
+		public Transform slicerEndEffector;
+		public float aoeExpandFactor = 5f;
+		public bool debug = false;
+
+		private bool _currentlySlicing = false;
+		private bool _previouslySlicing = false;
+		private VelocityTracker _velocityTracker;
+		private SlicingKnife _slicingKnife;
+
+		private void Start() {
+			_velocityTracker = GetComponent<VelocityTracker>();
+			_slicingKnife = GetComponent<SlicingKnife>();
+		}
+
+		[CanBeNull] private Tuple<Vector3, Vector3> sliceFirst = null;
+
+		private void onSlicingStart() {
+			sliceFirst = new Tuple<Vector3, Vector3>(slicerBeginEffector.position, slicerEndEffector.position);
+		}
+
+		private void triggerSlicing(Quadrilateral quad) {
+			quad = quad.ExpandedTop(aoeExpandFactor);
+			if (debug)
+			{
+				quad.Render();
+			}
+			Plane quadPlane = quad.plane;
+
+			foreach (GameObject objectToSlice in quad.GetCollisions("SliceTarget")) {
+				Plane overridenPlane = SlicingCrack.SnapCrackedGameObject(objectToSlice, quadPlane);
+				if (overridenPlane is null) {
+					continue;
+				}
+				_slicingKnife.Slice(objectToSlice, overridenPlane);
+			}
+		}
+
+		private void onSlicingStop() {
+			Debug.Log("Slicing stopped!");
+			if (sliceFirst is null) {
+				Debug.LogWarning("Slicing stopped but no points recorded!");
+				return;
+			}
+
+			Tuple<Vector3, Vector3> sliceLast =
+				new Tuple<Vector3, Vector3>(slicerBeginEffector.position, slicerEndEffector.position);
+			Quadrilateral quad = new Quadrilateral(
+				sliceFirst.Item1,
+				sliceLast.Item1,
+				sliceLast.Item2,
+				sliceFirst.Item2
+			);
+			triggerSlicing(quad);
+			sliceFirst = null;
+		}
+
+		private bool checkSlicing() {
+			Vector3 angVel = _velocityTracker.GetAngularVelocityEstimate();
+			_currentlySlicing = (angVel.magnitude > angVelocityThreshold);
+			if (_currentlySlicing && !_previouslySlicing) {
+				onSlicingStart();
+			}
+			else if (!_currentlySlicing && _previouslySlicing) {
+				onSlicingStop();
+			}
+
+			_previouslySlicing = _currentlySlicing;
+			return _currentlySlicing;
+		}
+
+		private void Update() {
+			checkSlicing();
+		}
+
+	}
+}
---
+++ Assets/SLICING/Internal/Knife.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EzySlice;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Slicing {
+
+	class SlicingKnife : MonoBehaviour {
+
+		public bool addColider = true;
+		public bool addRigidbodies = true;
+		public float pushForceMagnitude = 1f;
+		public float pushInitialInSeconds = 1f;
+		public int sliceCountLimit = 4;
+
+		[CanBeNull]
+		public GameObject[] Slice(GameObject objectToSlice, Plane plane) {
+			int sliceCount = SliceCounter.GetSliceCount(objectToSlice);
+			sliceCount++;
+			if (sliceCount >= sliceCountLimit) {
+				Destroy(objectToSlice);
+				return null;
+			}
+
+			GameObject[] hulls = createHulls(objectToSlice, plane.center, plane.normal);
+			if (hulls is null) {
+				return null;
+			}
+
+			Vector3 workaroundShift = GetWorkaroundShift(objectToSlice, hulls);
+			Destroy(objectToSlice);
+			foreach (GameObject hull in hulls) {
+				SliceCounter.SetSliceCount(hull, sliceCount);
+				hull.tag = "SliceTarget";
+				hull.layer = LayerMask.NameToLayer("Hulls");
+				hull.transform.position -= workaroundShift;
+			}
+
+			if (addRigidbodies) {
+				pushHulls(hulls, plane.normal, pushForceMagnitude, pushInitialInSeconds);
+			}
+
+			return hulls;
+		}
+
+		private void pushHulls(GameObject[] hulls, Vector3 planeNormal, float pushForceMagnitude,
+			float pushInitialInSeconds) {
+			float signum = 1f;
+			foreach (GameObject hull in hulls) {
+				Pushable.pushGameObject(hull, signum * pushForceMagnitude * planeNormal, pushInitialInSeconds);
+				signum *= -1;
+			}
+		}
+
+		[CanBeNull]
+		private GameObject[] createHulls(GameObject objectToSlice, Vector3 planeCenter, Vector3 planeNormal) {
+			if (!objectToSlice) {
+				Debug.LogError("Object to slice is null!");
+				return null;
+			}
+			Debug.Log("Slicing object " + objectToSlice.name);
+			Debug.Log("Plane center: " + planeCenter);
+			Debug.Log("Plane normal: " + planeNormal);
+			GameObject[] objects = objectToSlice.SliceInstantiate(planeCenter, planeNormal);
+			if (objects == null) {
+				Debug.Log("Slicing unsuccessful");
+				return null;
+			}
+
+			if (addColider) {
+				IgnoreHullCollisions.EnsureIgnored();
+				MeshUtils.AddMeshCollider(objects[0]);
+				MeshUtils.AddMeshCollider(objects[1]);
+			}
+
+			if (addRigidbodies) {
+				AddRigidbody(objects[0]);
+				AddRigidbody(objects[1]);
+			}
+
+			UpdateMaterial(objects[0].GetComponent<MeshRenderer>(), 0, ^1);
+			UpdateMaterial(objects[1].GetComponent<MeshRenderer>(), 0, ^1);
+			return objects;
+		}
+
+		private static void UpdateMaterial(MeshRenderer mr, Index source, Index target) {
+			Material[] mats = mr.materials;
+			mats[target] = mats[source];
+			mr.SetMaterials(mats.ToList());
+		}
+
+		private static void AddRigidbody(GameObject go) {
+			Rigidbody rb = go.AddComponent<Rigidbody>();
+			rb.useGravity = true;
+			rb.isKinematic = false;
+			rb.interpolation = RigidbodyInterpolation.Interpolate;
+			rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+		}
+
+		private static Vector3 GetWorkaroundShift(GameObject original, GameObject[] hull) {
+			// Very simple approximation
+			Vector3 meanHullPos = hull.Select(go => go.transform.position).Aggregate((a, b) => a + b) / hull.Length;
+			return meanHullPos - original.transform.position;
+		}
+	}
+
+	class IgnoreHullCollisions {
+		public static bool initialized = false;
+
+		public static void EnsureIgnored() {
+			if (initialized) {
+				return;
+			}
+
+			Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Hulls"), LayerMask.NameToLayer("Hulls"));
+			initialized = true;
+		}
+	}
+
+}
