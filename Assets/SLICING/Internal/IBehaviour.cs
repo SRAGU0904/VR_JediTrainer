3604e640507f77358bf9390966b1cc126c59e96c
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2025-05-24 18:08:14 +0200

    Delete external assets for slicing

--- Assets/SLICING/Internal/IBehaviour.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Slicing {
-	public interface ISlicingBehaviour {
-		public GameObject[] CreateHulls(GameObject objectToSlice, Vector3 planeCenter, Vector3 planeNormal);
-		public void PushHulls(GameObject[] hulls, Vector3 planeNormal);
-	}
-
-}
