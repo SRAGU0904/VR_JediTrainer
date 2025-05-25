18dbd368b97ec0b5cb3aa65821f18f4c75a6ba8b
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2025-05-25 17:31:56 +0200

    Fix multiline string

--- Assets/SLICING/Tutorial/Tut3/Tut3Counter.cs
+++ Assets/SLICING/Tutorial/Tut3/Tut3Counter.cs
@@ -8,6 +8,7 @@ public class Tut3Counter : MonoBehaviour {
 	public int numToComplete = 3;
 	public int myStage = -1;
 	public TMP_Text text;
+	[TextArea(3, 10)]
 	public string completedText = "Congrats!\nTutorial completed.\nTeleporting...";
 	private int counter = 0;
 
